211dcc3d05da18d1f98b5c0fe407004caa410e0c
Jason Fournier <jmf4nier@hotmail.com>
2019-01-23 17:56:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-  speed < 40 || speed > 60 ? "unsafe" : "safe"
-
+  if speed < 40 || speed > 60
+    "unsafe"
+  else
+    "safe"
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? "unsafe" : "safe"
 end
 	
 
